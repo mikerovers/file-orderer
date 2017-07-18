7afc83eea661decde6c7afeed6cfd9f1b5ae3189
Mike Rovers <mikerovers97@gmail.com>
2017-07-18 16:35:11 +0200

    Is wel af zo

---
+++ src/js/File.js
@@ -0,0 +1,13 @@
+/**
+ * Created by mike on 11-07-17.
+ */
+
+class File {
+    constructor(oldName, newName, id) {
+        this.oldName = oldName;
+        this.newName = newName;
+        this.index = id;
+    }
+}
+
+export default File;
