df2b1d250caae8381930c75e8aee4bc98d3a4bac
Mike Rovers <mikerovers97@gmail.com>
2017-07-18 16:58:46 +0200

    Build is gone

--- package/webpack.mix.js
+++
@@ -1,9 +0,0 @@
-let mix = require('laravel-mix');
-
-mix.js('src/js/main.js', 'dist/js/')  
-    .react('src/js/app.js', 'dist/js/')
-    .sass('src/css/app.scss', 'dist/css/app.css');
-
-mix.webpackConfig({
-    target: "electron-renderer"
-});
